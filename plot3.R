c081901bda47a8f03de2769fe437086fc843de02
JJPEREZ78 <juliojose.perez@gmail.com>
2015-05-09 01:16:03 +0100

    Final version for the 1st Programming Assesment Exploratory Data Analysis

--- plot3.R
+++ plot3.R
@@ -1,3 +1,24 @@
+## First we read the data
+
+library(read.table)
+
+Data<-read.table("household_power_consumption.txt",sep=";",na.string="?",head=TRUE,
+                 colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+
+# We capture the names on fields
+
+fields<-names(Data)
+
+# Add a new column with the format DataTime
+
+Data$datetime <- as.POSIXct(paste(Data$Date, Data$Time), format="%d/%m/%Y %H:%M:%S")
+
+# We select the data on 2007-02-01 and 2007-02-02
+
+Data2 <- subset(Data, datetime > as.POSIXct("2007-02-01 00:00:00") & datetime < as.POSIXct("2007-02-03 00:00:00"))
+
+## Plotting
+
 #Fist we open the media
 
 png("plot3.png",width=480,height=480,units="px")
